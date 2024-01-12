54e289d0c62ada6e4158ba2d97af99ebcbc86387
Heet Gudhka <heetgudhka24@gmail.com>
2024-01-12 13:03:14 +0530

    Unique JS Series

---
+++ 4. String to Number Conversion/script.js
@@ -0,0 +1,19 @@
+// String to number conversion
+
+console.log("1" + 2); // 12
+
+console.log(1 + "2"); // 12
+
+console.log("1" + 2 + 2); // 122
+
+console.log(1 + 2 + "2"); // 32
+
+// To implement certain operations first we should use parenthesis
+
+console.log((3 + 4) * 5 % 3); // 2
+
+// if the string is given first, all of them will be considered as a string and if the string is in the last position then the operations will take place first.
+
+console.log(true); // true
+
+console.log(+true); // 1 - bcoz conversion will take place we have incremented it
